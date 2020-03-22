3b671399ab3c9ba416354aa2a305c3188a0aae04
Juho Salminen <juho.salminen@gmail.com>
2020-03-22 09:10:06 +0200

    Add table to UI

--- app.R
+++ app.R
@@ -9,20 +9,29 @@
 
 library(shiny)
 library(leaflet)
-library(tidyverse)
+library(dplyr)
+library(stringr)
+
+LAST_TESTDATA <- strptime("2020-03-22 08:43:23", "%Y-%m-%d %H:%M:%S",
+                          tz = "Europe/Helsinki")
 
 # Read data from Google Sheets
 url <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vSCKudWJa8n5kQHvbEc2Ew17HWr1fco6awwWoIeRz7NrRkDA_A3__UGN3GP13Rb1GvF5x2hx1p1JZ4V/pub?output=csv"
 
 data <- readr::read_csv(url)
-data$Kaupunki <- toupper(data$Kaupunki)
+data$Kaupunki <- str_to_title(data$Kaupunki)
+data$Timestamp <- strptime(data$Timestamp, "%d/%m/%Y %H:%M:%S", 
+                           tz = "Europe/Helsinki") 
+
+# Filter out test data
+# data <- data %>% filter(Timestamp > LAST_TESTDATA)
 
 # Read post-codes
 postcodes <- readr::read_csv2("postcode_sample.csv")
 cities <- readr::read_tsv("kunnat.tsv")
 
 cities <- tidyr::separate(cities, Koordinaatit, c("lat", "lng"), sep = ", ")
-cities$Kunta <- toupper(cities$Kunta)
+cities$Kunta <- str_to_title(cities$Kunta)
 cities$lat <- as.numeric(gsub("°N", "", cities$lat))
 cities$lng <- as.numeric(gsub("°E", "", cities$lng))
 
@@ -47,6 +56,7 @@ ui <- fluidPage(
     # Show a plot of the generated distribution
     mainPanel(
         leafletOutput(outputId = "COVIDmap"), 
+        tableOutput("table_agg")
     
     )
 )
@@ -66,6 +76,10 @@ server <- function(input, output) {
                        label = ~label, 
                        color = "red", fillOpacity = 0.5)
     })
+    
+    output$table_agg <- renderTable(data_agg %>% 
+                                        select(Kaupunki, n) %>% 
+                                        rename(Tapauksia = n))
 }
 
 # Run the application 
